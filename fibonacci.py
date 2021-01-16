b8c78e98e1f888daf25a0559e0e33572563901fe
tonerqq <sypnowichp.backup@gmail.com>
2021-01-15 23:01:13 -0500

    completed the fibonacci sequence program

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,9 @@
-## FIBONACCI SEQUENCE 
-## @author  toner
-## @date    15 Jan 2021
+# FIBONACCI SEQUENCE 
+# @author  toner
+# @date    15 Jan 2021
 
+index = [0,1]
 
+for i in range(100):
+    index.append(index[i] + index[i+1])
+    print index[i+2]
